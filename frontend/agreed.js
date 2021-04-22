f3f83dc20c5c0f45cf54110e5a7512fa0e73626a
noyanyan <noyanyan@users.noreply.github.com>
2021-04-22 17:19:57 +0900

    apiパターンを追加　データを別ファイルに移行

--- frontend/agreed.js
+++ frontend/agreed.js
@@ -1,81 +1,74 @@
-const mockContestFactory = (object = {}) => ({
-  id: 'test01',
-  name: 'テストコンテスト 01',
-  statement: 'フロントエンド動作確認用のテストコンテストです。',
-  unixStartTime: 1609545600000,
-  startTime: '2021-01-02T00:00:00.000+00:00',
-  startTimeAMPM: '2021/01/02 09:00:00 AM',
-  unixEndTime: 1609547400000,
-  endTime: '2021-01-02T00:30:00.000+00:00',
-  endTimeAMPM: '2021/01/02 09:30:00 AM',
-  penalty: 0,
-  ratedBound: 0,
-  contestType: 'ATCODER',
-  ratingCalculated: false,
-  ...object,
-});
-
-const mockContestProblemFactory = (object = {}) => ({
-  contestName: 'test01',
-  point: 1,
-  statement: '答えはくそなぞなぞ',
-  indexOfContest: 0,
-  ...object,
-});
-
-const mockContestRankingListItemFactory = (object = {}) => ({
-  accountName: 'なぞなぞ界の tourist',
-  score: 2,
-  penalty: 0,
-  acceptList: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-  ranking: 1,
-  ...object,
-});
+import {
+  mockContestFactory,
+  mockAprilFoolContest,
+  mockAccountInfo,
+  mockAprilFoolContestProblems,
+  submitStatus,
+  mockContestRankingListItemFactory,
+} from './agreedData';
 
 // ここからAPI定義
 module.exports = [
   {
+    title: 'Latest Contest',
+    description:
+      '指定ページに対応するコンテストを最大10件返します。APIで与えられるJSONでコンテストは開催日順に与えられます。開催日が新しいものほどが先頭に並びます。トップページで使われます。',
     request: {
       path: '/api/contests/latest',
+      query: {
+        contest_page: '{:number}',
+      },
+      values: {
+        number: 0,
+      },
       method: 'GET',
     },
     response: {
+      headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+        withCredentials: true,
+      },
       body: [mockContestFactory({ id: 'test00', name: 'テストコンテスト 00' })],
     },
   },
 
   {
+    title: 'Contest Info',
+    description: '特定コンテストの情報を返します。',
     request: {
-      path: '/api/contests/test{:contest_no}',
+      path: '/api/contests/{:contest_title}',
+      values: {
+        contest_title: 'april2021',
+      },
     },
     response: {
-      body: mockContestFactory({
-        id: '{:contest_no}',
-        name: 'テストコンテスト {:contest_no}',
-      }),
+      body: mockAprilFoolContest,
     },
   },
 
   {
+    title: 'Problems',
+    description: 'get store information',
     request: {
       path: '/api/contests/{:contest_id}/problems',
+      value: {
+        contest_id: 'april2021',
+      },
     },
     response: {
-      body: Array(26)
-        .fill()
-        .map((_, i) =>
-          mockContestProblemFactory({
-            contestName: '{:contest_id}',
-            indexOfContest: i,
-          })
-        ),
+      body: mockAprilFoolContestProblems,
     },
   },
 
   {
+    title: 'Ranking in a Contest',
+    description: 'hoge',
     request: {
       path: '/api/contests/{:contest_id}/ranking',
-      query: { page: '0' },
+      query: { page: ':number' },
+      value: {
+        number: null,
+      },
     },
     response: {
       body: {
@@ -85,49 +78,64 @@ module.exports = [
             mockContestRankingListItemFactory({ ranking: no + 1 })
           ),
         acPerSubmit: [
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
+          { first: 222, second: 240 },
+          { first: 159, second: 200 },
+          { first: 130, second: 190 },
+          { first: 150, second: 162 },
+          { first: 142, second: 154 },
+          { first: 79, second: 140 },
+          { first: 38, second: 100 },
+          { first: 21, second: 32 },
+          { first: 0, second: 24 },
+          { first: 1, second: 18 },
         ],
         partAccountNum: 40,
         requestAccountRank: null,
       },
+    },
+  },
+
+  {
+    title: 'Submission: Failed',
+    description: '回答が与えられると、正解かどうか返します',
+    request: {
+      path: '/api/contests/submissions',
+      method: 'POST',
+      headers: {
+        contestId: 'april2021',
+        indexOfContest: '0',
+        statement: 'aa',
+      },
+    },
+    response: {
+      body: submitStatus,
     },
   },
   {
+    title: 'Account Info',
+    description: 'アカウントの情報を返します。',
     request: {
-      path: '/api/contests/{:contest_id}/ranking',
-      query: { page: '1' },
+      path: 'api/account/{:account}',
+      method: 'GET',
     },
     response: {
-      body: {
-        rankingList: Array(20)
-          .fill()
-          .map((_, no) =>
-            mockContestRankingListItemFactory({ ranking: no + 21 })
-          ),
-        acPerSubmit: [
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-          { first: 40, second: 40 },
-        ],
-        partAccountNum: 40,
-        requestAccountRank: null,
-      },
+      body: { mockAccountInfo },
     },
   },
 ];
+
+// api/submissions
+/**   {
+    title: 'api/',
+    description: '',
+    request: {},
+    response: {},
+  },
+  {
+    request: {},
+    response: {},
+  },
+  {
+    request: {},
+    response: {},
+  }, */
